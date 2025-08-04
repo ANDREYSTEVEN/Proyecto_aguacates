26c9d5d58673c0d99ea3221f30a22549c0bfd4b8
ANDREY STEVEN <andreyleon53@gmail.com>
2025-08-04 18:57:02 -0500

    Fix App.js routing + add GitHub Pages config

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 import Productos from "./pages/Productos";
